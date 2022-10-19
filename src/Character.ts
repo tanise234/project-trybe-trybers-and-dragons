57e38e3d72eb6fd444490ac1260cd92c75d1a03c
Tanise Santos <tanise234@gmail.com>
2022-10-18 23:59:12 -0300

    :pencil2: beginning: class Character

---
+++ src/Character.ts
@@ -0,0 +1,29 @@
+import Archetype from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race from './Races';
+
+export default class Character implements Fighter {
+//   private _race: Race;
+//   private _archetype: Archetype;
+//   private _maxLifePoints: number;
+//   private _lifePoints: number;
+//   private _strength: number;
+//   private _defense: number;
+//   private _dexterity: number;
+//   private _energy: Energy;
+//   public name: string;
+
+//   constructor(name: string) {
+//     this.name = name;
+//     this._race: Race;
+//     this._archetype: Archetype;
+//     this._maxLifePoints: number;
+//     this._lifePoints: number;
+//     this._strength: number;
+//     this._defense: number;
+//     this._dexterity = Math.random() * (10 - 1) + 1;
+//     this._energy: Energy;
+    
+  }
+}
