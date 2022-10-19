69e11db5416c4dd3182b88f61413c63d9875aa5e
Tanise Santos <tanise234@gmail.com>
2022-10-18 23:43:47 -0300

    :hammer: build: interface Fighter

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,15 @@
+import Energy from '../Energy';
+
+interface FighterBasic {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: Energy;
+}
+
+export default interface Fighter extends FighterBasic{
+  attack(enemy: FighterBasic): void;
+  special?(enemy: FighterBasic): void;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
+}
